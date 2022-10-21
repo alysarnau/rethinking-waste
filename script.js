a483fb56f25387fd094d490c40bf9e2112393cd0
alysvolatile <alice.g.cooper@gmail.com>
2022-10-21 10:43:21 -0400

    added active menu switching

--- script.js
+++ script.js
@@ -12,8 +12,16 @@ function smoothScroll(e) {
     });
 }
 
-// sticky header
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+const navList = document.querySelector(".nav-list")
+navList.addEventListener("click", (e) => {
+    const navLink = e.target.parentElement
+    if (navLink.classList.contains("link")) {
+        navList.querySelector(".active").classList.remove("active");
+        navLink.classList.add("active")
+    }
 })
