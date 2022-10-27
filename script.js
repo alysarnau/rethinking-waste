da88347a52d2f37e8d099c9cf94a990560a37012
alysvolatile <alice.g.cooper@gmail.com>
2022-10-27 10:48:55 -0400

    add scroll to top button and function

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ navList.addEventListener("click", (e) => {
         navLink.classList.add("active")
     }
 })
+
+const scrollButton = document.querySelector(".top")
+const rootElement = document.documentElement;
+
+document.addEventListener("scroll", showButton)
+scrollButton.addEventListener("click", scrollToTop)
+
+function showButton() {
+    const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+    if (rootElement.scrollTop / scrollTotal > 0.3) {
+        scrollButton.classList.add('show-btn')
+    } else {
+        scrollButton.classList.remove('show-btn')
+    }
+}
+
+function scrollToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
